81989b2c8ef56150c40b6813c6273b188d49515f
jeremynixon <jnixon2@gmail.com>
2014-08-06 16:55:18 -0400

    update database production

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'production',
+            'database'  => 'project4',
+            'username'  => 'nobuko',
+            'password'  => 'LVg1P4sD',
+        ),
+
+    ),
+
+);
